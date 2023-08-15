26559a41cf4afcd01272fb07c25dff644b218613
jivtodeprajwal27 <prajwal27.jivtode@gmail.com>
2023-08-15 20:53:01 +0530

    http-server on registration form

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,33 @@
-const hello=()=>{
-    console.log("Hello Github!")
-}
-hello()
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  }).listen(3000);
---
+++ http-server/index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const fs = require("fs");
+const args = process.argv.slice(2);
+
+if (args.length !== 2 || args[0] !== "--port") {
+  console.error("Usage: node index.js --port <port_number>");
+  process.exit(1);
+}
+
+const port = parseInt(args[1]);
+
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+http.createServer((request, response) => {
+  let url = request.url;
+  response.writeHead(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project.html":
+      response.write(projectContent);
+      response.end();
+      break;
+    case "/registration.html":
+      response.write(registrationContent);
+      response.end();
+      break;
+    default:
+      response.write(homeContent);
+      response.end();
+      break;
+  }
+}).listen(port);
+
