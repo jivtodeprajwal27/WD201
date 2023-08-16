c51b429b50aef55dea94a269c167d2867bd70701
jivtodeprajwal27 <prajwal27.jivtode@gmail.com>
2023-08-16 22:32:55 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -8,26 +8,27 @@ const todoList = () => {
     }
   
     const overdue = () => {
-        return all.filter(todo => !todo.completed && todo.dueDate < today);
-      }
+        return all.filter((todo) => !todo.completed && todo.dueDate < today);
+      };
     
-    const dueToday = () => {
-        return all.filter(todo => !todo.completed && todo.dueDate === today);
-      }
+      const dueToday = () => {
+        return all.filter((todo) => todo.dueDate === today);
+      };
     
-    const dueLater = () => {
-        return all.filter(todo => !todo.completed && todo.dueDate > today);
-      }
+      const dueLater = () => {
+        return all.filter((todo) => !todo.completed && todo.dueDate > today);
+      };
     
-    const toDisplayableList = (list) => {
+      const toDisplayableList = (list) => {
         let displayableList = '';
         for (const item of list) {
+          const checkbox = item.completed ? '[x]' : '[ ]';
           const title = item.title;
-          const formattedDueDate = (item.dueDate === today) ? "" : " (" + item.dueDate + ")";
-          displayableList += "- " + title + formattedDueDate + "\n";
+          const formattedDueDate = (item.dueDate === today) ? '' : ' ' + item.dueDate;
+          displayableList += checkbox + ' ' + title + formattedDueDate + '\n';
         }
         return displayableList;
-      }
+      };
   
     return {
       all,
