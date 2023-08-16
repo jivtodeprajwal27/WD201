7e9b5fa81fe39a27b57751f5c4b6ba88eff9c82f
jivtodeprajwal27 <prajwal27.jivtode@gmail.com>
2023-08-16 22:44:58 +0530

    update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -72,16 +72,15 @@ const todoList = () => {
   var overdues = todos.overdue()
   var formattedOverdues = todos.toDisplayableList(overdues)
   console.log(formattedOverdues)
-  console.log("\n")
+  console.log()
   
   console.log("Due Today")
   let itemsDueToday = todos.dueToday()
   let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
   console.log(formattedItemsDueToday)
-  console.log("\n")
+  console.log()
   
   console.log("Due Later")
   let itemsDueLater = todos.dueLater()
   let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
   console.log(formattedItemsDueLater)
-  console.log("\n\n")
