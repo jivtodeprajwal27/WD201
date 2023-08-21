81224f94f0dc08e59d713ecf3708877a9e8af4e6
jivtodeprajwal27 <prajwal27.jivtode@gmail.com>
2023-08-21 18:03:47 +0530

    http-server changes

--- http-server/index.js
+++ http-server/index.js
@@ -1,48 +1,48 @@
 const args = require("minimist")(process.argv.slice(2));
-p = args.port
+const portNumber = args.port;
 console.log(args.port); // prints the value of the --port option
 
 const http = require("http");
 const fs = require("fs");
 
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
+let contentForHome = "";
+let contentForProject = "";
+let contentForRegistration = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
   }
-  homeContent = home;
+  contentForHome = home;
 });
 fs.readFile("registration.html", (err, registration) => {
   if (err) {
     throw err;
   }
-  registrationContent = registration;
+  contentForRegistration = registration;
 });
 fs.readFile("project.html", (err, project) => {
   if (err) {
     throw err;
   }
-  projectContent = project;
+  contentForProject = project;
 });
 http.createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
       case "/project":
-        response.write(projectContent);
+        response.write(contentForProject);
         response.end();
         break;
-        case "/registration":
-        response.write(registrationContent);
+      case "/registration":
+        response.write(contentForRegistration);
         response.end();
         break;
       default:
-        response.write(homeContent);
+        response.write(contentForHome);
         response.end();
         break;
     }
   })
-  .listen(p);
+  .listen(portNumber);
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -39,48 +39,7 @@ const todoList = () => {
       dueLater,
       toDisplayableList
     };
-  };
-  
-  // ####################################### #
-  // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-  // ####################################### #
-  
-  const todos = todoList();
-  
-  const formattedDate = d => {
-    return d.toISOString().split("T")[0]
   }
+
+module.exports =todoList;
   
-  var dateToday = new Date()
-  const today = formattedDate(dateToday)
-  const yesterday = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() - 1))
-  )
-  const tomorrow = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() + 1))
-  )
-  
-  todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
-  todos.add({ title: 'Pay rent', dueDate: today, completed: true })
-  todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
-  todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
-  todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
-  
-  console.log("My Todo-list\n")
-  
-  console.log("Overdue")
-  var overdues = todos.overdue()
-  var formattedOverdues = todos.toDisplayableList(overdues)
-  console.log(formattedOverdues)
-  console.log()
-  
-  console.log("Due Today")
-  let itemsDueToday = todos.dueToday()
-  let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-  console.log(formattedItemsDueToday)
-  console.log()
-  
-  console.log("Due Later")
-  let itemsDueLater = todos.dueLater()
-  let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-  console.log(formattedItemsDueLater)
